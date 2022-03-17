ac4f82d05b62ecbad3435547e6c721aed745e125
aminuddinihsan20 <47616229+aminuddinihsan20@users.noreply.github.com>
2022-03-17 14:36:54 +0700

    Update and rename nad_surya.mk to aosp_surya.mk

--- aosp_surya.mk
+++ aosp_surya.mk
@@ -19,7 +19,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Nusantara stuff.
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# maintainer flag
+ARCANA_MAINTAINER := Inuo20
 
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -30,8 +33,6 @@ TARGET_USES_BLUR := true
 # Faceunlock
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
-# Official
-NAD_BUILD_TYPE := OFFICIAL
 
 # Pixel Charging
 USE_PIXEL_CHARGING := true
@@ -39,10 +40,16 @@ USE_PIXEL_CHARGING := true
 # Quick Tap
 TARGET_SUPPORTS_QUICK_TAP := true
 
+# pixel build flag, if not define build type is vanilla
+WITH_GAPPS := true
+
+# only applicaple to pixel builds (WITH_GAPPS,) default is lawnchair for vanilla build
+PREBUILT_LAWNCHAIR := true
+
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-PRODUCT_NAME := nad_surya
+PRODUCT_NAME := aosp_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
